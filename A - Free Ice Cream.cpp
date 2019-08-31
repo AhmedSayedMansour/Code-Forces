c50a6ee7fe04d79425915f16d0be30ec0b80cf99
Ahmed Sayed Mansour <36112767+AhmedSayedMansour@users.noreply.github.com>
2019-08-31 02:13:36 +0200

    Update A - Free Ice Cream.cpp

--- A - Free Ice Cream.cpp
+++ A - Free Ice Cream.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
-#define ms(a,v) memset(a,v,sizeof a)
+#include <stdio.h>
+#include <ctype.h>
+ 
 using namespace std;
-
-
 int main()
 {
-    int n,k,ans =0;
-    cin >> n>>k;
-    int x,y;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> x >> y;
-		ans += (y - x + 1);
-	}
-
-	((k - ans%k) == k ) ? cout<< 0 : cout <<k - ans%k ;
-	cout<< endl;
-
+    int n,x;
+    cin>>n>>x;
+    int d2[n],dod=0;
+    long long sum=x;
+    char d1[n];
+    for (int i=0;i<n;++i)
+    {
+        cin>>d1[i]>>d2[i];
+        if(d1[i]=='+')sum+=d2[i];
+        else
+        {
+            if (sum >=d2[i])sum-=d2[i];
+            else dod+=1;
+        }
+    }
+    cout<<sum<<' '<<dod;
     return 0;
 }
+ 
