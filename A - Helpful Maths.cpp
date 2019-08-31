87d98f8b4fc2aa27284498da97cb5bcb4b886953
Ahmed Sayed Mansour <36112767+AhmedSayedMansour@users.noreply.github.com>
2019-08-31 02:14:12 +0200

    Update A - Helpful Maths.cpp

--- A - Helpful Maths.cpp
+++ A - Helpful Maths.cpp
@@ -1,22 +1,32 @@
 #include <bits/stdc++.h>
-#define ms(a,v) memset(a,v,sizeof a)
+#include <stdio.h>
+#include <ctype.h>
+ 
 using namespace std;
-
-
 int main()
 {
-    int n,k,ans =0;
-    cin >> n>>k;
-    int x,y;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> x >> y;
-		ans += (y - x + 1);
-	}
-
-	((k - ans%k) == k ) ? cout<< 0 : cout <<k - ans%k ;
-	cout<< endl;
-
+    string s;
+    cin>>s;
+    int a[s.length()],count=0;
+    for(int i=0;i<s.length();++i)
+    {
+        if (s[i]=='1'){
+            a[count]=1;
+            ++count;
+        }
+        else if (s[i]=='2'){
+            a[count]=2;
+            ++count;
+       }
+        else if (s[i]=='3'){
+            a[count]=3;
+            ++count;
+        }
+    }
+    sort(a,a+count);
+    for(int i=0;i<count;i++){
+        cout<<a[i];
+        if(i!=count-1)cout<<'+';
+    }
     return 0;
 }
